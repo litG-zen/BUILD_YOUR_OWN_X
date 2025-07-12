e90b01dfa543bb063f2758600a0e62bc6eaf7f00
litG-zen <lalit.coder2296@gmail.com>
2025-07-12 18:25:36 +0530

    Single LinkedList : Nodecreation and Addition

--- blockchain_golang/linked_list_revision.go
+++ blockchain_golang/linked_list_revision.go
@@ -16,6 +16,32 @@ type LinkedNode struct {
 	next *LinkedNode
 }
 
+// Functionalities for LinkedList
+// Node initiation
+// Node addition
+// Node search by data
+// Node deletion
+// Node traversal
+// Node reversal
+
+func InitNode(data int) *LinkedNode {
+	return &LinkedNode{
+		data: data,
+		next: nil, // Explicitly setting next to nil (optional, since it's default)
+	}
+}
+
+func (l *LinkedNode) AddNode(data int) {
+	new_node := InitNode(data)
+	l.next = new_node
+}
+
 func main() {
-	fmt.Println("Hello! this is first commit for Linkedlist.")
+	first_node := InitNode(220396)
+	fmt.Printf("First node data :%v", first_node)
+
+	fmt.Println("\n New node addition")
+	first_node.AddNode(160896)
+	fmt.Printf("\n Updated LinkedList : %v %v", first_node.data, first_node.next)
+
 }
